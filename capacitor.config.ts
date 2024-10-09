df1a4cd93387e47c435072266ec2e6f95fde6d8b
Leojg018 <133281276+Leojg018@users.noreply.github.com>
2024-10-09 14:31:53 -0300

    firebase

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'trabajo-practico',
+  appId: 'tp.int.caminapp',
+  appName: 'caminapp',
   webDir: 'www'
 };
 
